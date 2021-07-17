09fd2e93cf61e62838f58da5bbe5209de855fdae
nadeem590 <nadeem@leewayhertz.com>
2021-07-17 09:35:36 -0700

    demo

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,12 @@ import {Redirect, Switch} from "react-router";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import {connect} from "react-redux";
-import {Login, SignUp} from "./modules";
+// import {Forgot} from "./modules";
+
+import {Login} from "./modules";
+import Forgot from './modules/forgot';
+import Email from './modules/forgot/emailComponent';
+import Dashboard from './modules/dashboard/dashboardComponent'
 import {history} from "./managers/history";
 import BaseComponent from "./modules/baseComponent";
 
@@ -21,7 +26,10 @@ class Routes extends BaseComponent {
                 <Router history={history}>
                     <Switch>
                         <Route exact path={'/'} component={Login}/>
-                        <Route exact path={'/sign-up'} component={SignUp}/>
+                        <Route exact path={'/forgot-password'} component={Forgot}/>
+                        <Route exact path={'/email-sent'} component={Email}/>
+                        <Route exact path={'/dashboard'} component={Dashboard}/>
+
                         <Redirect exact from='*' to="/"/>
                     </Switch>
                 </Router>
