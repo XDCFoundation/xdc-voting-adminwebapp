3344180fd972a919f65c8b949b41f28308ee14bc
nadeem590 <nadeem@leewayhertz.com>
2022-01-06 17:19:39 +0530

    bugs done

--- src/modules/dashboard/DemoForm.js
+++ src/modules/dashboard/DemoForm.js
@@ -205,6 +205,7 @@ export default function CustomizedSnackbars(props) {
     setProposal(false);
     setAddAddress("");
     setStateValues(false);
+    setEmailError("")
   };
 
   const handleClose = (event, reason) => {
